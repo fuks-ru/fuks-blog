fb9af8b1ec00803f199c9bcd3b7f61e288ae3871
Dmitry Fuks <difuks@gmail.com>
2022-04-14 22:22:48 +0300

    feat: Перенести в нужное место IndexController

--- entries/blog-frontend/src/server/Pages/PagesModule.ts
+++ entries/blog-frontend/src/server/Pages/PagesModule.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 
-import { IndexController } from 'blog-frontend/pages/_500/IndexController';
+import { IndexController } from 'blog-frontend/pages/IndexController';
 import { CategoryController } from 'blog-frontend/pages/categories/CategoryController';
 import { CoreBackendModule } from 'blog-frontend/server/CoreBackend/CoreBackendModule';
 
