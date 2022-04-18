446b2371c29a89df7cbbdfcc1f10ad7351ced12e
Dmitry Fuks <difuks@gmail.com>
2022-04-18 14:33:34 +0300

    feat: Копировать скрипт деплоя на сервер и выполнять его

--- .github/workflows/.deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker pull difuks/fuks-blog
-docker pull difuks/fuks-blog-nginx
-docker stop fuks-blog  &>/dev/nul
-docker stop fuks-blog-nginx  &>/dev/nul
-docker run --name fuks-blog --rm --network="my-blog" -d difuks/fuks-blog
-sleep 10
-docker run --name fuks-blog-nginx --rm --network="my-blog" -d difuks/fuks-blog-nginx
