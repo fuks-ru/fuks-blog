c242ce44435534c30b1333f4c0d7313a5c03164e
Dmitry Fuks <difuks@gmail.com>
2022-04-19 14:43:21 +0300

    feat(admin): Фикс пути до основного бэка

--- entries/admin-frontend/src/common/constants.ts
+++ entries/admin-frontend/src/common/constants.ts
@@ -1,6 +1,5 @@
 /**
  * Адрес основного бэкенда.
  */
-export const BLOG_BACKEND_URL = process.env.NODE_ENV
-  ? 'http://localhost:3001'
-  : '/';
+export const BLOG_BACKEND_URL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : '/';
