d1fce2831ffb68daaceac389ab060b54e24e6212
Dmitry Fuks <difuks@gmail.com>
2022-04-22 20:31:53 +0300

    fix(auth-backend): Исправить установку env

--- .github/workflows/.deploy.sh
+++ .github/workflows/.deploy.sh
@@ -12,14 +12,20 @@ docker stop fuks-blog-nginx  &>/dev/nul
 docker stop fuks-blog-auth-postgres  &>/dev/nul
 
 docker run --name fuks-blog-auth-postgres \
-    --rm -d \
-    -e POSTGRES_PASSWORD="${FUKS_BLOG_AUTH_POSTGRES_PASSWORD}" \
-    -e POSTGRES_USER="${FUKS_BLOG_AUTH_POSTGRES_USER}" \
-    -e POSTGRES_DB=auth \
-    -e PGDATA=/var/lib/postgresql/data/pgdata \
-    -v "${FUKS_BLOG_AUTH_POSTGRES_VOLUME}":/var/lib/postgresql/data \
-     --network="my-blog" \
-    postgres:14.2-alpine
-docker run --name fuks-blog --rm --network="my-blog" -e PERSONAL_NPM_TOKEN="${PERSONAL_NPM_TOKEN}" -d difuks/fuks-blog
+  --rm -d \
+  -e POSTGRES_PASSWORD="${FUKS_BLOG_AUTH_POSTGRES_PASSWORD}" \
+  -e POSTGRES_USER="${FUKS_BLOG_AUTH_POSTGRES_USER}" \
+  -e POSTGRES_DB=auth \
+  -e PGDATA=/var/lib/postgresql/data/pgdata \
+  -v "${FUKS_BLOG_AUTH_POSTGRES_VOLUME}":/var/lib/postgresql/data \
+  --network="my-blog" \
+  postgres:14.2-alpine
+docker run --name fuks-blog \
+  --rm -d \
+  -e FUKS_BLOG_AUTH_POSTGRES_PASSWORD="${FUKS_BLOG_AUTH_POSTGRES_PASSWORD}" \
+  -e FUKS_BLOG_AUTH_POSTGRES_USER="${FUKS_BLOG_AUTH_POSTGRES_USER}" \
+  -e FUKS_BLOG_AUTH_JWT_SECRET="${FUKS_BLOG_AUTH_JWT_SECRET}" \
+  --network="my-blog" \
+  difuks/fuks-blog
 sleep 10
 docker run --name fuks-blog-nginx --rm --network="my-blog" -d difuks/fuks-blog-nginx
