842f33f739f93df969737988a118b758e0bf3bd9
Dmitry Fuks <difuks@gmail.com>
2022-05-23 18:15:12 +0300

    feat: Make cookies optional for Request

--- packages/common/src/modules/Logger/middlewares/LoggerMiddleware.ts
+++ packages/common/src/modules/Logger/middlewares/LoggerMiddleware.ts
@@ -15,6 +15,10 @@ interface ICookie {
   sessionId: string;
 }
 
+interface IRequest extends Omit<Request, 'cookies'> {
+  cookies?: ICookie;
+}
+
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   public constructor(private readonly logger: Logger) {}
@@ -25,10 +29,10 @@ export class LoggerMiddleware implements NestMiddleware {
    * Устанавливает уникальный request id для запроса и куку для уникальной
    * сессии. С целью объединения нескольких логов в рамках одного запроса и сессии.
    */
-  public use(req: Request, res: Response, next: NextFunction): void {
+  public use(req: IRequest, res: Response, next: NextFunction): void {
     requestContext.set(`${REQUEST_CONTEXT_ID}:${REQUEST_ID_KEY}`, v4());
 
-    const sessionId = (req.cookies as ICookie).sessionId || v4();
+    const sessionId = req.cookies?.sessionId || v4();
 
     res.cookie('sessionId', sessionId);
 
