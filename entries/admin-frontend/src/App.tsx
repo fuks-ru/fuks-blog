c206b058dc83a63c5bc8ec0a43f7019d0c6d26a7
Dmitry Fuks <difuks@gmail.com>
2022-05-30 20:19:29 +0300

    feat: test

--- entries/admin-frontend/src/App.tsx
+++ entries/admin-frontend/src/App.tsx
@@ -3,4 +3,4 @@ import { FC } from 'react';
 /**
  * Главный компонент админки.
  */
-export const App: FC = () => <div>Hello from Admin frontend</div>;
+export const App: FC = () => <div>Hello world</div>;
