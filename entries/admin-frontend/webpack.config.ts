1b08002848d8a4e585d3965f442f471f61249bff
Dmitry Fuks <difuks@gmail.com>
2022-06-15 13:56:57 +0300

    fix(auth-frontend): Fix env variable

--- entries/admin-frontend/webpack.config.ts
+++ entries/admin-frontend/webpack.config.ts
@@ -21,6 +21,8 @@ const plugins: WebpackPluginInstance[] = [
   new NodePolyfillPlugin(),
   new EnvironmentPlugin({
     NODE_ENV: process.env.NODE_ENV,
+    FUKS_BLOG_AUTH_GOOGLE_CLIENT_ID:
+      process.env.FUKS_BLOG_AUTH_GOOGLE_CLIENT_ID,
   }),
 ];
 
