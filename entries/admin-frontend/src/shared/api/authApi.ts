cd0971c993687fe8e37a3b77c85df76d4403437e
Dmitry Fuks <difuks@gmail.com>
2022-07-15 14:48:53 +0300

    fix(admin-frontend): Mutations fix

--- entries/admin-frontend/src/shared/api/authApi.ts
+++ entries/admin-frontend/src/shared/api/authApi.ts
@@ -129,126 +129,6 @@ type IApi<ReducerPath extends string> = Api<
   typeof reactHooksModuleName | typeof coreModuleName
 >;
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const updateMutation = <
-  Methods extends Partial<Record<keyof OperationMethods, TMethodData>>,
-  ReducerPath extends string,
->(
-  method: string,
-  build: EndpointBuilder<BaseQueryFn<IQueryArgs>, string, string>,
-  methods: Methods,
-  api: IApi<ReducerPath>,
-) => [
-  method,
-  build.mutation<
-    TApiResponse<keyof OperationMethods>,
-    {
-      body: TApiBody<keyof OperationMethods>;
-      params: TApiArgs<keyof OperationMethods>;
-    }
-  >({
-    query: ({ body, params }) => ({
-      method: method as keyof OperationMethods,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      body,
-      params,
-    }),
-    onQueryStarted: async ({ body, params }, { dispatch, queryFulfilled }) => {
-      const listFunction = Object.entries(methods).find(
-        ([, data]) => data.type === 'getList',
-      )?.[0];
-
-      if (!listFunction) {
-        await queryFulfilled;
-
-        return;
-      }
-
-      const patchResult = dispatch(
-        api.util.updateQueryData(
-          listFunction as never,
-          undefined as never,
-          (draft) => {
-            if (!('id' in params)) {
-              return;
-            }
-
-            const updatedData = (
-              draft as Array<{
-                id: string;
-              }>
-            ).find((item) => item.id === params.id);
-
-            if (!updatedData) {
-              return;
-            }
-
-            Object.assign(updatedData, body);
-          },
-        ),
-      );
-
-      try {
-        await queryFulfilled;
-      } catch {
-        patchResult.undo();
-      }
-    },
-  }),
-];
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-const deleteMutation = <
-  Methods extends Partial<Record<keyof OperationMethods, TMethodData>>,
-  ReducerPath extends string,
->(
-  method: string,
-  build: EndpointBuilder<BaseQueryFn<IQueryArgs>, string, string>,
-  methods: Methods,
-  api: IApi<ReducerPath>,
-) => [
-  method,
-  build.mutation<void, string>({
-    query: (id: string) => ({
-      method: method as keyof OperationMethods,
-      body: {},
-      params: {
-        id,
-      },
-    }),
-    onQueryStarted: async (id: string, { dispatch, queryFulfilled }) => {
-      const listFunction = Object.entries(methods).find(
-        ([, data]) => data.type === 'getList',
-      )?.[0];
-
-      if (!listFunction) {
-        await queryFulfilled;
-
-        return;
-      }
-
-      const patchResult = dispatch(
-        api.util.updateQueryData(
-          listFunction as never,
-          undefined as never,
-          (draft) =>
-            (
-              draft as Array<{
-                id: string;
-              }>
-            ).filter((item) => item.id !== id),
-        ),
-      );
-
-      try {
-        await queryFulfilled;
-      } catch {
-        patchResult.undo();
-      }
-    },
-  }),
-];
-
 interface IOptions<
   Methods extends Partial<Record<keyof OperationMethods, TMethodData>>,
   ReducerPath extends string,
@@ -272,6 +152,119 @@ export const createAuthApi = <
   never,
   typeof reactHooksModuleName | typeof coreModuleName
 > => {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const updateMutation = (
+    method: string,
+    build: EndpointBuilder<BaseQueryFn<IQueryArgs>, string, string>,
+  ) => [
+    method,
+    build.mutation<
+      TApiResponse<keyof OperationMethods>,
+      {
+        body: TApiBody<keyof OperationMethods>;
+        params: TApiArgs<keyof OperationMethods>;
+      }
+    >({
+      query: ({ body, params }) => ({
+        method: method as keyof OperationMethods,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        body,
+        params,
+      }),
+      onQueryStarted: async (
+        { body, params },
+        { dispatch, queryFulfilled },
+      ) => {
+        const listFunction = Object.entries(options.methods).find(
+          ([, data]) => data.type === 'getList',
+        )?.[0];
+
+        if (!listFunction) {
+          await queryFulfilled;
+
+          return;
+        }
+
+        const patchResult = dispatch(
+          (api as unknown as IApi<ReducerPath>).util.updateQueryData(
+            listFunction as never,
+            undefined as never,
+            (draft) => {
+              if (!('id' in params)) {
+                return;
+              }
+
+              const updatedData = (
+                draft as Array<{
+                  id: string;
+                }>
+              ).find((item) => item.id === params.id);
+
+              if (!updatedData) {
+                return;
+              }
+
+              Object.assign(updatedData, body);
+            },
+          ),
+        );
+
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+    }),
+  ];
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const deleteMutation = (
+    method: string,
+    build: EndpointBuilder<BaseQueryFn<IQueryArgs>, string, string>,
+  ) => [
+    method,
+    build.mutation<void, string>({
+      query: (id: string) => ({
+        method: method as keyof OperationMethods,
+        body: {},
+        params: {
+          id,
+        },
+      }),
+      onQueryStarted: async (id: string, { dispatch, queryFulfilled }) => {
+        const listFunction = Object.entries(options.methods).find(
+          ([, data]) => data.type === 'getList',
+        )?.[0];
+
+        if (!listFunction) {
+          await queryFulfilled;
+
+          return;
+        }
+
+        const patchResult = dispatch(
+          (api as unknown as IApi<ReducerPath>).util.updateQueryData(
+            listFunction as never,
+            undefined as never,
+            (draft) =>
+              (
+                draft as Array<{
+                  id: string;
+                }>
+              ).filter((item) => item.id !== id),
+          ),
+        );
+
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      },
+    }),
+  ];
+
   const getCrudEndpoints =
     (
       methods: Methods,
@@ -282,21 +275,11 @@ export const createAuthApi = <
       Object.fromEntries(
         Object.entries(methods).map(([method, { type }]) => {
           if (type === 'update') {
-            return updateMutation(
-              method,
-              build,
-              methods,
-              api as unknown as IApi<ReducerPath>,
-            );
+            return updateMutation(method, build);
           }
 
           if (type === 'delete') {
-            return deleteMutation(
-              method,
-              build,
-              methods,
-              api as unknown as IApi<ReducerPath>,
-            );
+            return deleteMutation(method, build);
           }
 
           return [
